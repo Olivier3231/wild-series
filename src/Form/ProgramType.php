8a5f4bcc9c88093859986de416f084339f571cee
Olivier3231 <maffrolivier74@gmail.com>
2021-06-21 18:29:59 +0200

    modify form

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -8,18 +8,27 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormView;
+use App\Entity\Actor;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ProgramType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            
             ->add('title', TextType::class)
             ->add('synopsis', TextType::class)
             ->add('poster', TextType::class)
             ->add('country', TextType::class)
             ->add('year', NumberType::class)
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+            ])
             ->add('category', null, ['choice_label' => 'name'])
         ;
     }
